1d7b05895dc28d0c8fdcef6e2749dfccb81f3004
gustavo-rethink <gustavo@re-think.consulting>
2019-05-31 00:19:51 +0200

    Delete Pessoa

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -12,12 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.repository.PessoaRepository;
@@ -45,5 +40,11 @@ public class PessoaResource {
 		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
 		 return pessoa.isPresent() ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
 	}
-	
+
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		pessoaRepository.deleteById(codigo);
+	}
+
 }
